af5772f4b7128a3bd88e0bfe3823c5f5ec89f3f2
xombiesaur <dchaseb23@gmail.com>
2014-09-02 16:51:19 -0400

    First Upload

---
+++ StopWatch.java
@@ -0,0 +1,15 @@
+/*
+CSE 17
+David Brewster
+DCB316
+Homework 2 	DEADLINE: September 7th, 2014
+Program Discription; Stop Watch And Math Test
+*/
+
+public class StopWatch {
+	public static void main(String [] args) {
+		userInterface();
+	}
+	public static userInterface{
+		
+}
