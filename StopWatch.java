8586e7fc4ae419d0810684dc40344d31f968b0a1
xombiesaur <dchaseb23@gmail.com>
2014-09-03 14:37:03 +0000

    update 2

--- StopWatch.java
+++ StopWatch.java
@@ -5,21 +5,38 @@ DCB316
 Homework 2 	DEADLINE: September 7th, 2014
 Program Discription; Stop Watch And Math Test
 */
-
+public class 
 public class StopWatch {
-	boolean running;
+	public boolean running;
+	public int startTime;
+	public int timedMS;
+	
 	public quizStart(){
 		startTime = System.currentTimeMillis;
 		timedMs = 0;
-		running = true;
+		running = false;
 	}
+	/* Main method that starts the user interface
+	*/
 	public static void main(String [] args) {
+		stopWatch programStart = New Stopwatch();
 		userInterface();
 	}
-	public static userInterface{
+	/* Asks the user for their name and starts the quiz
+	*/
+	public static void userInterface(){
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter your name:  ");
+		String userName = console.next(); 		
+	}
+	/* creates a quiz for the user*/
+	public static int quiz(){
+		
+	}
+	/*checks to see how much time has passed since the start
+	*/
+	public static int getElapsedTime(){
 	
 	}
-	public static getElapsedTime(){
 	
-	}
 }
